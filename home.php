fe988f61b94dc426e0634bce6654ecbcac77beed
Divyam <divyamchoudhary@hotmail.com>
2019-11-17 17:37:44 +0530

    home.php made more secure

--- home.php
+++ home.php
@@ -1,4 +1,10 @@
-
+<?php
+  session_start();
+  if(isset($_SESSION['username']) && !empty($_SESSION['username'])) 
+  {
+      header("location: welcome.php");
+  }
+?>
 
 <!DOCTYPE html>
 <html lang="en">
